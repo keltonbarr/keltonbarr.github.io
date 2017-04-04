e9435a18eb80d2bacf7fe5e3a4e34cc7c8214c27
keltonbarr <bar11084@byui.edu>
2017-04-04 12:45:02 -0600

    final

--- acme/js/code.js
+++ acme/js/code.js
@@ -1,48 +1,61 @@
-$.ajax({
-    url: "/acme/js/acme.json",
-    dataType: "json",
-    success: function (data) {
+$(document).ready(function () {
 
-        var output = '';
-        $.each(data, function (key, val) {
-            output += '<li>';
-            output += '<a href = " ' + this.path + ' ">' + key + '</a>';
-            output += '</li>';
+        var jsonData;
+
+        document.getElementById("product-content").style.display = "none";
+
+        $.getJSON("/acme/js/acme.json", function (data) {
+            jsonData = data;
+            console.log(jsonData);
+            var output = '<ul>';
+            $.each(data, function (key, value) {
+                output += '<li>';
+                output += '<a href="//google.com" title="' + key + '">' + key + '</a>';
+                output += '</li>';
+            });
+            output += '</ul>';
+            $("#page-nav").html(output);
         });
-        $("nav .flexlist").append(output);
-    }
-});
 
+        $("#page-nav").on("click", "a", function (evt) {
+            evt.preventDefault();
+            var pageName = $(this).text();
+            console.log("You clicked: " + pageName);
 
-$(function () {
-    $("nav .flexlist").on("click", ":not(:first-child) a", function (evt) {
-        evt.preventDefault();
+            if (pageName == "Home") {
+                document.getElementById("home-content").style.display = "inline";
+                document.getElementById("product-content").style.display = "none";
+                $("title").text("ACME");
+            } else {
 
-        var jsonAcme = $(this).text();
-        console.log(jsonAcme);
-        $.ajax({
-            url: "/acme/js/acme.json",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                name = data[jsonAcme].name;
-                price = data[jsonAcme].price;
-                description = data[jsonAcme].description;
-                manufacturer = data[jsonAcme].manufacturer;
-                reviews = data[jsonAcme].reviews;
-                product_img = data[jsonAcme].path;
+                document.getElementById("home-content").style.display = "none";
+                document.getElementById("product-content").style.display = "inline";
 
-
-                $('#name').text(name);
-                $('#price').text(price);
-                $('#description').text(description);
-                $('#manufacturer').text('Made by: ' + manufacturer);
-                $('#reviews').text('Rating: ' + reviews + ' Stars');
-
-                $('#product_img').html('<img src="' + product_img + '" alt="'+ name +'">');
+                var name = jsonData[pageName].name;
+                var path = jsonData[pageName].path;
+                var description = jsonData[pageName].description;
+                var manufacturer = jsonData[pageName].manufacturer;
+                var price = jsonData[pageName].price;
+                var reviews = jsonData[pageName].reviews;
+                console.log(name);
+                console.log(path);
+                console.log(description);
+                console.log(manufacturer);
                 console.log(price);
-                $('#home').fadeOut();
+                console.log(reviews);
+
+                $("title").text("ACME " + pageName);
+                $("#prodtitle").html(name);
+                $("#product-name").text(name);
+                $("#prod-image").css("background-image", "url(" + path + ")");
+                var output = '';
+                output += "<li>" + description + "</li> <br>";
+                output += '<li><strong>Made by: </strong>' + manufacturer + '</li>' + '<br>';
+                output += '<li><strong>Reviews: </strong>' + reviews + '/5 stars</li>';
+                output += '<li><h2>Price: $' + price + '</h2></li>';
+                $("#prod-description").html(output);
+                $("#prod-description h2").css("color", "#de2226");
             }
+
         });
-    });
-});
+})
